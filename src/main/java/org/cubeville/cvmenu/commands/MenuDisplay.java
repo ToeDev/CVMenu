f6140047cc964f30cfbc9d90276214d39c2f3e53
ToeDev <73237279+ToeDev@users.noreply.github.com>
2022-05-03 16:06:40 -0400

    Updated display command to allow execution from console

--- src/main/java/org/cubeville/cvmenu/commands/MenuDisplay.java
+++ src/main/java/org/cubeville/cvmenu/commands/MenuDisplay.java
@@ -2,9 +2,10 @@ package org.cubeville.cvmenu.commands;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
-import org.cubeville.commons.commands.Command;
+import org.cubeville.commons.commands.BaseCommand;
 import org.cubeville.commons.commands.CommandExecutionException;
 import org.cubeville.commons.commands.CommandParameterString;
 import org.cubeville.commons.commands.CommandResponse;
@@ -17,7 +18,7 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
 
-public class MenuDisplay extends Command {
+public class MenuDisplay extends BaseCommand {
 
     private final CVMenu plugin;
 
@@ -28,8 +29,7 @@ public class MenuDisplay extends Command {
         this.plugin = plugin;
     }
 
-    @Override
-    public CommandResponse execute(Player player, Set<String> flags, Map<String, Object> parameters, List<Object> baseParameters) throws CommandExecutionException {
+    public CommandResponse execute(CommandSender sender, Set<String> flags, Map<String, Object> parameters, List<Object> baseParameters) throws CommandExecutionException {
         MenuManager manager = plugin.getMenuManager();
         if(!manager.menuExists((String) baseParameters.get(0))) {
             throw new CommandExecutionException(ChatColor.RED + "Menu " + ChatColor.GOLD + baseParameters.get(0) + ChatColor.RED + " doesn't exist!");
@@ -42,10 +42,13 @@ public class MenuDisplay extends Command {
             Player p = Bukkit.getPlayer((String) parameters.get("player"));
             Inventory displayInv = menu.getDisplayInventory(p);
             Objects.requireNonNull(p).openInventory(displayInv);
-            return new CommandResponse("");
+            return new CommandResponse(ChatColor.GREEN + "Menu " + ChatColor.GOLD + baseParameters.get(0) + ChatColor.GREEN + " opened for " + ChatColor.GOLD + p.getName());
         }
-        Inventory displayInv = menu.getDisplayInventory(player);
-        player.openInventory(displayInv);
-        return new CommandResponse("");
+        if(!(sender instanceof Player)) {
+            return new CommandResponse(ChatColor.RED + "You cannot open a menu from console!");
+        }
+        Inventory displayInv = menu.getDisplayInventory((Player) sender);
+        ((Player) sender).openInventory(displayInv);
+        return new CommandResponse(ChatColor.GREEN + "Menu " + ChatColor.GOLD + baseParameters.get(0) + ChatColor.GREEN + " opened");
     }
 }
